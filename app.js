25390a2eea490688b8342c41ac362b09478728d7
WinnietheDEV <WorldWhyWin@gmail.com>
2023-02-09 20:26:25 +0700

    simple get route

--- app.js
+++ app.js
@@ -32,6 +32,9 @@ app.use(helmet());
 app.use(cors());
 app.use(xss());
 
+app.get("/", (req, res) => {
+  res.send("job api");
+});
 app.use("/api/v1/jobs", authUser, jobsRouter);
 app.use("/api/v1/auth", authRouter);
 // extra packages
